ff343d3875e607282da62fcacc17b6b0662a5e52
Daniele Lodola <daniele.lodola+GITHUB@gmail.com>
2019-03-26 23:34:39 +0100

    Debug

---
+++ javascripts/libs.js
@@ -0,0 +1,11 @@
+---
+# The front matter is required, even if empty
+---
+
+{% include_relative modernizr.foundation.js %}
+{% include_relative jquery-2.1.0.js %}
+{% include_relative terrific-1.1.1.js %}
+{% include_relative jquery.url.js %}
+{% include_relative jquery.cookie.js %}
+{% include_relative json2.js %}
+{% include_relative foundation.js %}
